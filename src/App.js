b3b532d1e8dba130716a1bf6817a9538d4ba18a1
su1415 <dummy@example.com>
2024-07-24 12:40:47 +0900

    feat: TODOリストの表示

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TodoList from "./components/TodoList"
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "sample1" },
+    { id: 2, text: "sample2" },
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo List</h1>
+      <TodoList todos={ todos } />
     </div>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoItem({ todo }) {
+  return (
+    <li>{ todo.text }</li>
+  );
+}
+
+export default TodoItem;
