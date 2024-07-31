7d89441ac6d83ec863a63b397d49253c1fa886af
su1415 <dummy@example.com>
2024-07-31 15:45:13 +0900

    style: classを追加

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -16,7 +16,7 @@ function TodoItem({ todo, onDeleteTodo, onSaveEditTodo, onToggleComplete }) {
 
   return (
     <li className={`list-group-item d-flex justify-content-between align-items-center ${todo.completed ? "completed" : ""}`}>
-      <div className="d-flex align-items-center">
+      <div className="d-flex">
         <input
           type="checkbox"
           className="me-2 custom-checkbox"
@@ -24,19 +24,23 @@ function TodoItem({ todo, onDeleteTodo, onSaveEditTodo, onToggleComplete }) {
           onChange={ () => onToggleComplete(todo.id) }
           disabled={ isEditing }
         />
+      </div>
+      <div className="d-flex align-items-center flex-grow-1">
         { isEditing ? (
           <>
             <input
               type="text"
               value={ editTodo.text }
               onChange={(e) => setEditTodo({ ...editTodo, text: e.target.value })}
-              className="form-control me-2"
+              className="form-control me-2 flex-grow-1"
             />
             <input
               type="date"
               value={ editTodo.dueDate }
               onChange={(e) => setEditTodo({ ...editTodo, dueDate: e.target.value })}
-              className="form-control"
+              className="form-control due-date"
+              style={{ width: '150px' }}
+              disabled={ !isEditing }
             />
           </>
         ) : (
@@ -45,16 +49,16 @@ function TodoItem({ todo, onDeleteTodo, onSaveEditTodo, onToggleComplete }) {
           </span>
         )}
       </div>
-      <div>
+      <div className="d-flex align-items-center">
         { isEditing ? (
           <>
             <button
-              className="btn btn-success btn-sm me-2"
+              className="btn btn-success btn-sm me-2 fixed-width"
               onClick={ handleSaveClick }>
               Save
             </button>
             <button
-              className="btn btn-secondary btn-sm"
+              className="btn btn-secondary btn-sm fixed-width"
               onClick={ handleEditToggle }>
               Cancel
             </button>
@@ -62,12 +66,12 @@ function TodoItem({ todo, onDeleteTodo, onSaveEditTodo, onToggleComplete }) {
         ) : (
           <>
             <button
-              className="btn btn-warning btn-sm me-2"
+              className="btn btn-warning btn-sm me-2 fixed-width"
               onClick={ handleEditToggle }>
               Edit
             </button>
             <button
-              className="btn btn-danger btn-sm"
+              className="btn btn-danger btn-sm fixed-width"
               onClick={ () => onDeleteTodo(todo.id) }>
               Delete
             </button>
